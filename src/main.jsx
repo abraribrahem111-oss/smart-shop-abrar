a47af6f1ab11271e17e1324aa4c93d523ae01ecf
Abrar Ibrahem <abraribrahem111@gmail.com>
2026-08-16 21:24:12 +0300

    fix router basename for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./App.css";
-import CartProvider from "./Context/CartContext.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <CartProvider>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/smart-shop-abrar">
       <App />
-    </CartProvider>
-  </StrictMode>
+    </BrowserRouter>
+  </React.StrictMode>
 );
