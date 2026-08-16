e74a39b593bca875b90189a406e8509831667363
Abrar Ibrahem <abraribrahem111@gmail.com>
2026-08-16 20:50:54 +0300

    fix login import case

--- src/Components/PupLogin.jsx
+++ src/Components/PupLogin.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { CgProfile } from "react-icons/cg";
-import Login from "../Pages/Login"
+import Login from "../Pages/LogIn"
 
 const PupLogin = ({ isLoggedIn }) => {
 
