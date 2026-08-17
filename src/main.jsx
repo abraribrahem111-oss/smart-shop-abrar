9a57518cf390a0e7e49261006dc73b70face05db
Abrar Ibrahem <abraribrahem111@gmail.com>
2026-08-17 18:01:10 +0300

    Configure gh-pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./App.css";
+import  CartProvider  from "./Context/CartContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/smart-shop-abrar">
-      <App />
+    <BrowserRouter >
+      <CartProvider>
+        <App />
+      </CartProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
